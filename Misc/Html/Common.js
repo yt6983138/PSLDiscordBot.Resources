6a882b41d3acc113947a29f05f0df19eec908858
static_void <83499886+yt6983138@users.noreply.github.com>
2026-08-01 23:44:03 +0800

    Fix numbering and lower bound for Phi 3

--- Misc/Html/Common.js
+++ Misc/Html/Common.js
@@ -420,3 +420,24 @@ function FormatDateOffset(dateBefore, dateAfter, shouldShowAfter = 1, digits = 1
 function RandomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
+
+function DeepEquals(x, y) {
+    if (x === y) {
+        return true;
+    }
+    else if ((typeof x == "object" && x != null) && (typeof y == "object" && y != null)) {
+        if (Object.keys(x).length !== Object.keys(y).length)
+            return false;
+
+        for (let prop in x) {
+            if (y.hasOwnProperty(prop)) {
+                if (!DeepEquals(x[prop], y[prop]))
+                    return false;
+            }
+            else return false;
+        }
+
+        return true;
+    }
+    else return false;
+}
